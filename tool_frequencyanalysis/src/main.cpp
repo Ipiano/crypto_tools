36c1cacb1f67921fbb4945a88c8be34eb6284361
Andrew Stelter <andrew.d.stelter@gmail.com>
2017-10-24 12:40:09 -0600

    Added builds to gitignore Added /Books to gitignore as location to store test data Fixed frequency analysis tool makefile Finished frequency analysis tool

--- tool_frequencyanalysis/src/main.cpp
+++ tool_frequencyanalysis/src/main.cpp
@@ -4,9 +4,18 @@
 #include <fstream>
 #include <iomanip>
 #include <cctype>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 using namespace frequency;
+
+struct frequency_count
+{
+    char letter;
+    uint64_t count;
+    double percent;
+};
 
 int main(int argc, char** argv)
 {
@@ -16,7 +25,14 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    uint64_t frequencies[255] = {0};
+    vector<frequency_count> frequencies(255);
+    char i = 0;
+    for(frequency_count& f : frequencies)
+    {
+        f.letter = i++;
+        f.count = 0;
+        f.percent = 0;
+    }
 
     for(uint32_t i=1; i<argc; i++)
     {
@@ -24,7 +40,8 @@ int main(int argc, char** argv)
         if(fin)
         {
             cout << "Processing " << argv[i] << "..." << endl;
-            countFrequencies(fin, frequencies);
+            countFrequencies<vector<frequency_count>::iterator, frequency_count>
+                (fin, frequencies.begin(), [](frequency_count& f){ f.count++; });
         }
         else
         {
@@ -35,18 +52,23 @@ int main(int argc, char** argv)
     string line = string(50, '-');
     cout << endl << line << endl;
 
-    uint64_t total;
-    for(uint32_t i=0; i<255; i++)
-        total += frequencies[i];
+    uint64_t total = 0;
+    for(const frequency_count& f : frequencies)
+        total += f.count;
+
+    for(frequency_count& f : frequencies)
+        f.percent = f.count/(double)total * 100;
+
+    sort(frequencies.begin(), frequencies.end(), [](const frequency_count& l, const frequency_count& r){ return l.percent > r.percent; });
 
     cout << total << " total characters read" << endl << line << endl << endl;
 
-    for(uint32_t i=0; i < 255; i++)
+    for(const frequency_count& f : frequencies)
     {
-        if(frequencies[i])
+        if(f.count)
         {
-            cout << "\t" << (char)i << " (" << setw(3) << i << ")" << "\t" << setw(10) << frequencies[i];
-            cout << "\t" << setprecision(5) << frequencies[i]/(double)total*100 << "%" << endl;
+            cout << "\t" << (char)f.letter << " (" << setw(4) << (int)f.letter << ")" << "\t" << setw(10) << f.count;
+            cout << "\t" << setprecision(5) << f.percent << "%" << endl;
         }
             
     }
