98d9f2fbd9f533fee005f068ad0d3faf66610e51
Andrew Stelter <andrew.d.stelter@gmail.com>
2017-12-08 02:34:00 -0700

    Added des3, des4 crackers to des4 tool

--- tool_des4/src/main.cpp
+++ tool_des4/src/main.cpp
@@ -13,9 +13,9 @@ using namespace des4;
 
 enum class Input{None, File, Term};
 enum class Output{None, File, Term};
-enum class Mode{None, Encrypt, Decrypt};
+enum class Mode{None, Encrypt, Decrypt, Crack3, Crack4};
 
-bool processArgs(int argc, char** argv, Input& inMode, Output& outMode, Mode& op, string& key, string& input, string& output);
+bool processArgs(int argc, char** argv, Input& inMode, Output& outMode, Mode& op, uint64_t& trials, string& key, string& input, string& output);
 void help(string name, string msg = "");
 string charsFromHex(string input);
 string hexFromChars(string input);
@@ -23,6 +23,7 @@ string hexFromChars(string input);
 int main(int argc, char** argv)
 {
     string key, input, output;
+    uint64_t trials;
     Input inputMode;
     Output outputMode;
     Mode operation;
@@ -35,94 +36,136 @@ int main(int argc, char** argv)
     istream* inStream = &inText;
     ostream* outStream = &cout;
 
-    if(!processArgs(argc, argv, inputMode, outputMode, operation, key, input, output))
+    if(!processArgs(argc, argv, inputMode, outputMode, operation, trials, key, input, output))
     {
         return 1;
     }
 
-    if(key.find_first_not_of("01") != string::npos || key.size() != 9)
+    if(operation == Mode::Encrypt || operation == Mode::Decrypt)
     {
-        help(argv[0], "Key must contain exactly 9 characters from the set [\'0\', \'1\']");
-        return 3;
-    }
-
-    uint16_t key_val = 0;
-    for(int i=0; i<9; i++)
-        key_val |= ((key[i] - '0') << 8 - i);
-
-    if(inputMode == Input::File)
-    {
-        inFile.open(input, ios::binary);
-        if(!inFile)
+        if(key.find_first_not_of("01") != string::npos || key.size() != 9)
         {
-            help(argv[0], "Unable to open input file " + input);
-            return 2;
+            help(argv[0], "Key must contain exactly 9 characters from the set [\'0\', \'1\']");
+            return 3;
         }
 
-        inStream = &inFile;
-    }
-    else
-    {
-        try
+        uint16_t key_val = 0;
+        for(int i=0; i<9; i++)
+            key_val |= ((key[i] - '0') << 8 - i);
+
+        if(inputMode == Input::File)
         {
-            inText << charsFromHex(input);  
-        }catch(exception)
-        {
-            help(argv[0], input + " is not a valid hexadecimal value");
-            return 4;
-        }
-    }
+            inFile.open(input, ios::binary);
+            if(!inFile)
+            {
+                help(argv[0], "Unable to open input file " + input);
+                return 2;
+            }
 
-    if(outputMode == Output::File)
-    {
-        outFile.open(output, ios::binary | ios::trunc);
-        if(!outFile)
-        {
-            help(argv[0], "Unable to open output file " + output);
-            inFile.close();
-            return 2;
-        }
-
-        outStream = &outFile;
-    }
-
-    function<uint16_t(uint16_t, const uint16_t&, const uint16_t&)> op = (operation == Mode::Encrypt ? encrypt : decrypt);
-
-    uint8_t blocks[3];
-    while(*inStream)
-    {
-        blocks[0] = blocks[1] = blocks[2] = 0;
-        inStream->read((char*)blocks, 3);
-
-        if(!*inStream && !inStream->gcount()) break;
-
-        uint16_t block1 = (blocks[0] << 4) | (blocks[1] >> 4);
-        uint16_t block2 = (blocks[1] << 8) | blocks[2];
-
-        block1 = op(block1, key_val, 4);
-        block2 = op(block2, key_val, 4);
-
-        blocks[0] = (block1 & 0xFF0) >> 4;
-        blocks[1] = ((block1 & 0xF) << 4) | ((block2 & 0xF00) >> 8);
-        blocks[2] = (block2 & 0xFF);
-        
-        if(outputMode == Output::File)
-        {
-            outStream->write((char*)blocks, 3);
+            inStream = &inFile;
         }
         else
         {
-            *outStream << hexFromChars(string((char*)blocks, 3));
+            try
+            {
+                inText << charsFromHex(input);  
+            }catch(exception)
+            {
+                help(argv[0], input + " is not a valid hexadecimal value");
+                return 4;
+            }
+        }
+
+        if(outputMode == Output::File)
+        {
+            outFile.open(output, ios::binary | ios::trunc);
+            if(!outFile)
+            {
+                help(argv[0], "Unable to open output file " + output);
+                inFile.close();
+                return 2;
+            }
+
+            outStream = &outFile;
+        }
+
+        function<uint16_t(uint16_t, const uint16_t&, const uint16_t&)> op = (operation == Mode::Encrypt ? encrypt : decrypt);
+
+        uint8_t blocks[3];
+        while(*inStream)
+        {
+            blocks[0] = blocks[1] = blocks[2] = 0;
+            inStream->read((char*)blocks, 3);
+
+            if(!*inStream && !inStream->gcount()) break;
+
+            uint16_t block1 = (blocks[0] << 4) | (blocks[1] >> 4);
+            uint16_t block2 = (blocks[1] << 8) | blocks[2];
+
+            block1 = op(block1, key_val, trials);
+            block2 = op(block2, key_val, trials);
+
+            blocks[0] = (block1 & 0xFF0) >> 4;
+            blocks[1] = ((block1 & 0xF) << 4) | ((block2 & 0xF00) >> 8);
+            blocks[2] = (block2 & 0xFF);
+            
+            if(outputMode == Output::File)
+            {
+                outStream->write((char*)blocks, 3);
+            }
+            else
+            {
+                *outStream << hexFromChars(string((char*)blocks, 3));
+            }
+        }
+
+        inFile.close();
+        outFile.close();
+    }
+    else
+    {
+        cout << "The cracker will give you a 12-bit block to encrypt as 3 hexadecimal digits" << endl;
+        cout << "Encrypt the block and enter the 12-bit block that results as 3 hexadecimal digits" << endl;
+        function<uint16_t(uint16_t)> box =
+        [](uint16_t block)
+        {
+            char block_c[2] = {(char)(block >> 8), (char)(block & 0xFF)};
+            cout << "Encrypt " << hexFromChars(string((char*)block_c, 2)).substr(1) << endl << "> " << flush;
+
+            string in;
+            bool valid;
+            do
+            {
+                valid = true;
+                cin >> in;
+                if(in.size() != 3 || in.find_first_not_of("0123456789abcdef") != string::npos)
+                {
+                    cout << "Enter 3 hexadecimal digits" << endl << "> " << flush;
+                    valid = false;
+                }
+            }while(!valid);
+
+            in = charsFromHex(in);
+            block = ((in[0] << 4) & 0x0FF0) | ((in[1] >> 4) & 0x000F);
+            return block;
+        };
+
+        function<uint16_t(function<uint16_t(uint16_t)>)> cracker = crack3;
+        if(operation == Mode::Crack4) cracker = std::bind(crack4, placeholders::_1, trials);
+
+        uint16_t key;
+        try{
+            key = cracker(box);
+            cout << "Key: " << hex << key << dec << endl;
+        }catch(exception& ex){
+            cout << "Unable to crack: " << ex.what() << endl;
         }
     }
-
-    inFile.close();
-    outFile.close();
 
     return 0;
 }
 
-bool processArgs(int argc, char** argv, Input& inMode, Output& outMode, Mode& op, string& key, string& input, string& output)
+bool processArgs(int argc, char** argv, Input& inMode, Output& outMode, Mode& op, uint64_t& trials, string& key, string& input, string& output)
 {
     inMode = Input::None;
     outMode = Output::None;
@@ -213,21 +256,69 @@ bool processArgs(int argc, char** argv, Input& inMode, Output& outMode, Mode& op
         {
             if(op != Mode::None)
             {
-                help(argv[0], "Choose exactly one operation [-e, -d]");
+                help(argv[0], "Choose exactly one operation [-e, -d, -c3, -c4]");
                 return false;
             }
 
             op = Mode::Encrypt;
+
+            try{
+                trials = stoull(argv[++i]);
+            }catch(exception& ex){
+                help(argv[0], "Specify number of rounds with -e [rounds]");
+                return false;
+            }
         }
         else if(arg == "-d")
         {
             if(op != Mode::None)
             {
-                help(argv[0], "Choose exactly one operation [-e, -d]");
+                help(argv[0], "Choose exactly one operation [-e, -d, -c3, -c4]");
                 return false;
             }
 
             op = Mode::Decrypt;
+
+            try{
+                trials = stoull(argv[++i]);
+            }catch(exception& ex){
+                help(argv[0], "Specify number of rounds with -d [rounds]");
+                return false;
+            }
+        }
+        else if(arg == "-c3")
+        {
+            if(op != Mode::None)
+            {
+                help(argv[0], "Choose exactly one operation [-e, -d, -c3, -c4]");
+                return false;
+            }
+
+            op = Mode::Crack3;
+        }
+        else if(arg == "-c4")
+        {
+            if(op != Mode::None)
+            {
+                help(argv[0], "Choose exactly one operation [-e, -d, -c3, -c4]");
+                return false;
+            }
+
+            op = Mode::Crack4;
+            if(i < argc-1)
+            {
+                try{
+                    trials = stoull(argv[++i]);
+                }catch(exception& ex){
+                    help(argv[0], "Specify number of trials with -c4 [trials]");
+                    return false;
+                }
+            }
+            else
+            {
+                help(argv[0], "Specify number of trials with -c4 [trials]");
+                return false;
+            }
         }
         else
         {
@@ -242,16 +333,19 @@ bool processArgs(int argc, char** argv, Input& inMode, Output& outMode, Mode& op
         return false;
     }
 
-    if(inMode == Input::None)
+    if(op == Mode::Encrypt || op == Mode::Decrypt)
     {
-        help(argv[0], "Choose exactly one input mode [-it, -if]");
-        return false;
-    }
+        if(inMode == Input::None)
+        {
+            help(argv[0], "Choose exactly one input mode [-it, -if]");
+            return false;
+        }
 
-    if(outMode == Output::None)
-    {
-        help(argv[0], "Choose exactly one output mode [-ot, -of]");
-        return false;
+        if(outMode == Output::None)
+        {
+            help(argv[0], "Choose exactly one output mode [-ot, -of]");
+            return false;
+        }
     }
 
     return true;
